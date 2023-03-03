82a54bdd879f2b9c746ebd64fc823aad88d114a4
EmiroMunoz01 <emiro_2001@icloud.com>
2023-03-02 22:25:01 -0500

    Metodo para editar un elemento de la base de datos

--- API Rest Nodejs/app.js
+++ API Rest Nodejs/app.js
@@ -54,7 +54,6 @@ app.get("/api/articulos/:id", (req, res) => {
   );
 });
 
-
 //añadiremos un articulo a la base de datos
 app.post("/api/articulos", (req, res) => {
   let data = {
@@ -72,6 +71,26 @@ app.post("/api/articulos", (req, res) => {
     }
   });
 });
+
+//editar articulo
+
+app.put("/api/articulos/:id", (req, res) => {
+  let id = req.params.id;
+  let descripcion = req.body.descripcion;
+  let precio = req.body.precio;
+  let stock = req.body.stock;
+  let sql =
+    "UPDATE articulos SET descripcion = ?, precio = ?, stock = ? WHERE id = ?";
+
+  conexion.query(sql, [descripcion, precio, stock, id], (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.send(results);
+    }
+  });
+});
+
 //el primer parametro es el puerto, el segundo parametro es una f(x) que imprima una salida para verificar que el servidor se ejecuta, o capturar un error
 
 //crearemos una variable para nuestro puerto
